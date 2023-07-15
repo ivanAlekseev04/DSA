2c2144b9e0429fc39d4a92bdbbf6c0eb3c6c9caf
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2023-07-15 11:19:02 +0300

    Update Sort.h

--- Algorithms/Sort.h
+++ Algorithms/Sort.h
@@ -151,3 +151,32 @@ void countingSort(int* arr, size_t size)
             i++;
     }
 }
+
+// Counting sort - 2 variant
+void countSort(int* arr, int size) {
+    int max = *max_element(arr, arr + size);
+
+    int* buff = new int[max + 1]{};
+
+    for(int i = 0; i < size; i++) {
+        buff[arr[i]]++;
+    }
+
+    for(int i = 1; i < max + 1; i++) {
+        buff[i] += buff[i - 1];
+    }
+
+    int* sorted = new int[size]{};
+
+    for(int i = size - 1; i >= 0; i--) {
+        sorted[buff[arr[i]] - 1] = arr[i];
+        buff[arr[i]]--;
+    }
+
+    for(int i = 0; i < size; i++) {
+        arr[i] = sorted[i];
+    }
+
+    delete buff;
+    delete sorted;
+}
