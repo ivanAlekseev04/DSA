1a7836cf47312dd1fc7923f578530aae513b29f7
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2023-07-16 09:45:10 +0300

    Update Search.h
    Binary search recursive, ternary search recursive added

--- Algorithms/Search.h
+++ Algorithms/Search.h
@@ -1,46 +1,92 @@
 #pragma once
 
-int binarySearch(const int* arr, size_t left, size_t right, int val) {
-    int idxNotFoundElement = -1;
+// Iterative binary search
+int binarySearch(int* arr, int left, int right, int toFind) { // "right" need to be size - 1
+    int notFound = -1;
 
-    while(left < right) {
-        size_t mid = left + (right - left) / 2;
+    while(left <= right) {
+        int mid = left + ((right - left) / 2);
 
-        if(arr[mid] == val) {
+        if(arr[mid] == toFind) {
             return mid;
-        }
-        else if(arr[mid] > val) {
-            right = mid;
-        }
-        else if(arr[mid] < val) {
+        } else if(arr[mid] > toFind) {
+            right = mid - 1;
+        } else {
             left = mid + 1;
-        }
+        }   
     }
 
-    return idxNotFoundElement;
+    return notFound;
 }
 
-int ternarySearch(const int* arr, int left, int right, int val) {
-    while(right >= left) {
-        int firstMid = left + (right - left) / 3;
-        int secondMid = right - (right - left) / 3;
-
-        if(arr[firstMid] == val)
-            return firstMid;
-        if(arr[secondMid] == val)
-            return secondMid;
-        
-        if(val < arr[firstMid])
-            right = firstMid - 1;
-        else if(val > arr[secondMid])
-            left = secondMid + 1;
-        else {
-            left = firstMid + 1;
-            right = secondMid - 1;
-        }
+//Recursive binary search
+int binarySearch(int* arr, int left, int right, int toFind) { // "right" need to be size - 1
+    if(left > right) {
+        return -1;
     }
 
-    return -1;
+    int mid = left + ((right - left) / 2);
+
+    if(arr[mid] == toFind) {
+        return mid;
+    } else if(arr[mid] > toFind) {
+        binarySearch(arr, left, mid - 1, toFind);
+    } else {
+        binarySearch(arr, mid + 1, right, toFind);
+    }
+}
+
+// Iterative ternary search
+int ternarySearch(int* arr, int left, int right, int toFind) { // "right" need to be size - 1
+    int notFound = -1;
+
+    while(left <= right) {
+        int mid1 = left + ((right - left) / 3);
+        int mid2 = right - ((right - left) / 3);
+
+        if(arr[mid1] == toFind) {
+            return mid1;
+        } else if(arr[mid2] == toFind) {
+            return mid2;
+        }
+        
+        if(toFind < arr[mid1]) {
+            right = mid1 - 1;
+        } else if(toFind > arr[mid2]) {
+            left = mid2 + 1;
+        } else {
+            left = mid1 + 1;
+            right = mid2 - 1;
+        }  
+    }
+
+    return notFound;
+}
+
+// Recursive ternary search
+int ternarySearch(int* arr, int left, int right, int toFind) {  // "right" need to be size - 1
+    int notFound = -1;
+
+    if(left > right) {
+        return notFound;
+    }
+
+    int mid1 = left + ((right - left) / 3);
+    int mid2 = right - ((right - left) / 3);
+
+    if(arr[mid1] == toFind) {
+        return mid1;
+    } else if(arr[mid2] == toFind) {
+        return mid2;
+    }
+        
+    if(toFind < arr[mid1]) {
+        ternarySearch(arr, left, mid1 - 1, toFind);
+    } else if(toFind > arr[mid2]) {
+        ternarySearch(arr, mid2 + 1, right, toFind);
+    } else {
+        ternarySearch(arr, mid1 + 1, mid2 - 1, toFind);
+    }  
 }
 
 int jumpSearch(const int* arr, int left, int right, int val) {
