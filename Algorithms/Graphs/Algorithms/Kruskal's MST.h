4629dc5fceebf0c6c47b2703bb317f248f79312b
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2023-07-26 13:23:31 +0300

    Update Kruskal's MST.h
    More efficient implementation of Kruskal's algorithm for finding MST

--- Algorithms/Graphs/Algorithms/Kruskal's MST.h
+++ Algorithms/Graphs/Algorithms/Kruskal's MST.h
@@ -2,98 +2,109 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
-int v, e;
- 
-struct Component {
-    int parent;
-    int rank;
- 
-    Component() : parent(-1), rank(0) {}
-    Component(int parent, int rank) : parent(parent), rank(rank) {}
-};
+
 struct Node {
     int from;
     int to;
     int weight;
- 
-    Node() : from(-1), to(-1), weight(0) {}
+
+    Node() : from(0), to(0), weight(0) {}
     Node(int from, int to, int weight) : from(from), to(to), weight(weight) {}
 };
-struct Comparator {
-    bool operator ()(const Node& f, const Node& s) const {
-        return (f.weight > s.weight);
-    }
+
+struct RankedNode {
+    int parent;
+    int rank;
+
+    RankedNode() : parent(0), rank(0) {}
+    RankedNode(int parent, int rank) : parent(parent), rank(rank) {}
 };
- 
-priority_queue<Node, vector<Node>, Comparator> nodes;
-vector<Component> components;
+
+int E, V;
+vector<Node> adj;
+vector<RankedNode> ranks;
+
+// For the result
 vector<Node> mst;
- 
-void setUpComponents() {
-    components = vector<Component>(v);
- 
-    for(size_t i = 0; i < v; i++) {
-        components[i] = Component(i, 0);
+int MSTcost = 0;
+
+// Preparation/initialization
+void initializeRanks() {
+    for(int i = 0; i < V; i++) {
+        ranks.push_back(RankedNode(i, 0));
     }
 }
-int find(int val) {
-    if(components[val].parent != val) {
-        components[val].parent = find(components[val].parent);
+void initializeEdges() {
+    int from, to, weight;
+
+    for(int i = 0; i < E; i++) {
+        cin >> from >> to >> weight;
+        adj.push_back(Node(from, to, weight));
     }
- 
-    return components[val].parent;
 }
-int kruskal() {
-    int minPathWeight = 0;
- 
-    for(size_t i = 0; i < e; i++) {
-        if(mst.size() == v - 1) {
-            break;
+
+// Initial functions for the Kruskal's MST
+int findParent(int from) {
+    if(ranks[from].parent != from) { // Diging down to the parents of "from" till there is no parents of the "from"
+        ranks[from].parent = findParent(ranks[from].parent); // Path-compression technique; works similarly to remove/insert in BST
+    }
+
+    return ranks[from].parent;
+}
+void unionEdges(int from, int to) {
+    if(ranks[from].rank < ranks[to].rank) {
+        ranks[from].parent = to;
+    } else if(ranks[from].rank > ranks[to].rank) {
+        ranks[to].parent = from;
+    } else {
+        ranks[from].parent = to;
+        ranks[to].rank++;
+    }
+}
+bool kruskal() {
+    sort(adj.begin(), adj.end(), [](Node& f, Node& s) {return f.weight < s.weight;}); // Sorting edges in increasing order by "weight"
+
+    int i = 0, j = 0;
+
+    while(i < V - 1 && j < E) {
+        int fromP = findParent(adj[j].from);
+        int toP = findParent(adj[j].to);
+        int weight = adj[j].weight;
+
+        j++; // Iterator for the edges in graph
+
+        if(fromP == toP) {
+            continue;
         }
- 
-        Node toOperate = nodes.top();
-        nodes.pop();
- 
-        int root1 = find(toOperate.from);
-        int root2 = find(toOperate.to);
- 
-        if(root1 != root2) {
-            if(components[root1].rank < components[root2].rank) {
-                components[root1].parent = root2;
-            }
-            else if(components[root1].rank > components[root2].rank) {
-                components[root2].parent = root1;
-            }
-            else {
-                components[root1].parent = root2;
- 
-                components[root2].rank++;
-            }
- 
-            minPathWeight += toOperate.weight;
-            mst.push_back(toOperate);
-        }
+
+        unionEdges(fromP, toP);
+        MSTcost += weight;
+        mst.push_back(adj[j - 1]);
+
+        i++; // Incrementing count of edges already existed in MST 
     }
- 
-    return minPathWeight;
+
+    if(i != V - 1) // Final check, whether MST was formed completely
+        return false;
+
+    return true; 
+}   
+
+int main() {
+    cin >> V >> E;
+
+    initializeRanks();
+    initializeEdges();
+
+    if(!kruskal()) {
+        cout << "MST can't be formed for this graph\n";
+        return -1;
+    }
+
+    cout << "\nMST cost is: " << MSTcost << '\n';
+    for(auto it : mst) {
+        cout << "From: " << it.from << " To: " << it.to << " Weight: " << it.weight << '\n';
+    }
+
+    return 0;
 }
- 
-// int main(int argc, char** argv) {
-//     cin >> v >> e;
- 
-//     setUpComponents();
- 
-//     int from, to, weight;
-//     for(size_t i = 0; i < e; i++) {
-//         cin >> from >> to >> weight;
- 
-//         nodes.push(Node(from, to, weight));
-//     }
- 
-//     cout << kruskal() << '\n';
- 
-//     for(size_t i = 0; i < mst.size(); i++) {
-//         cout << mst[i].from << " " << mst[i].to << " " << mst[i].weight << '\n';
-//     }
-// }
