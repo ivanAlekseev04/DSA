e49ca79683a5f681878d93057fcce0fb2be8a4a2
Ivan Alekseev <89075086+JustImprove@users.noreply.github.com>
2023-08-21 19:50:46 +0300

    Update Heap_sort.h

--- Algorithms/Sorting algorithms/Heap_sort.h
+++ Algorithms/Sorting algorithms/Heap_sort.h
@@ -1,13 +1,19 @@
 // Time complexity: O(nlog(n))
 void heapSortAscending(vector<int>& v) {
+    auto iter = v.begin();
+    
     for(int i = 0; i < v.size() - 1; i++) {
-        make_heap(next(v.begin(), i), v.end(), greater<int>());
+        make_heap(iter, v.end(), greater<int>());
+        iter++;
     }
 }
 
 void heapSortDescending(vector<int>& v) {
+    auto iter = v.begin();
+    
     for(int i = 0; i < v.size() - 1; i++) {
-        make_heap(next(v.begin(), i), v.end()); // make_heap() makes max_heap by default
+        make_heap(iter, v.end()); // make_heap() makes max_heap by default
+        iter++;
     }
 }
 
